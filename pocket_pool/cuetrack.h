7f7d8517905fb1b20133866e85d7ce7b9511e464
ShawnHan <shawnhan1029@gmail.com>
2017-04-15 13:49:40 -0400

    half done with merging

--- pocket_pool/cuetrack.h
+++ pocket_pool/cuetrack.h
@@ -2,16 +2,22 @@
 #define CUETRACK_H
 
 #include<QPair>
-
+using namespace cv;
 
 class Cuetrack
 {
 public:
     Cuetrack();
     QPair<double, double> startTracking(QPair<double, double> ballPos);
+    vector<Point> spiral_marker_detect(Mat img, Scalar target, size_t n, int number, vector<Point> pre_vec);
+    vector<Point> detect_marker(Mat img, Scalar target, size_t n, int &number);
 private:
     double vX;
     double vY;
+    
+    bool filter(Point *start, Point *end, int m, int n, int Type);
+    vector<Point> detect_marker(Mat img, Scalar target, size_t n, int &number);
+    vector<Point> predict_marker(vector<Point> pre, vector<Point> cur)
 };
 
 #endif // CUETRACK_H
